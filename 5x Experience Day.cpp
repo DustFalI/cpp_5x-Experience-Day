5a07353492b392ef5626b7876999a0eda7311e92
DustFalI <1540500607@qq.com>
2025-03-29 22:22:09 +0800

    Update 5x Experience Day.cpp

--- 5x Experience Day.cpp
+++ 5x Experience Day.cpp
@@ -15,7 +15,7 @@ int main()
 			int lose = op[i][1], win = op[i][2], xx = op[i][3];
 			if(xx <= j)
 			{
-				dp[i][j] = max(dp[i-1][j] + lose, win + dp[i-1][j-xx]);
+				dp[i][j] = max(dp[i-1][j] + lose, win + dp[i-1][j-xx]);//这里注意与常规01背包的不同，本题0药剂也要比，就是输掉获得的经验
 			}
 			else
 			{
