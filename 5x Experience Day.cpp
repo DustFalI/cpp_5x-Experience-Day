79063eda1916a9fe1b0b47b8f70c81e24f841a58
DustFalI <1540500607@qq.com>
2025-03-29 22:06:28 +0800

    Add files via upload

---
+++ 5x Experience Day.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long n, x, dp[1005][1005], op[1005][5] = {0};
+int main()
+{
+	cin >> n >> x;
+	for(int i = 1; i <= n; i++)
+	{
+		cin >> op[i][1] >> op[i][2] >> op[i][3];
+	}
+	for(int i = 1; i <= n; i++)
+	{
+		for(int j = 0; j <= x; j++)
+		{
+			int lose = op[i][1], win = op[i][2], xx = op[i][3];
+			if(xx <= j)
+			{
+				dp[i][j] = max(dp[i-1][j] + lose, win + dp[i-1][j-xx]);
+			}
+			else
+			{
+				dp[i][j] = dp[i-1][j] + lose;
+			}
+		}
+	}
+	cout << 5 * dp[n][x];
+	return 0;
+}
